f636351d7774b25f8da950382f440a96793b0307
Melissa Fraas <melissafraas@Melissas-MacBook-Air.local>
2021-04-18 00:28:56 -0400

    read in csv file in main.py

---
+++ PyPoll/Analysis/main.py
@@ -0,0 +1,18 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+poll_csv = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv')
+#02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv
+
+
+
+# Read in the CSV file
+with open(poll_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+    print(header)
+    

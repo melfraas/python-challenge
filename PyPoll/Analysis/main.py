945eb5578744990659281f69616a460bf1898478
Melissa Fraas <melissafraas@Melissas-MacBook-Air.local>
2021-04-25 16:27:20 -0400

    new csv output

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -1,7 +1,6 @@
 import os
 import csv
 
-
 # Path to collect data from the Resources folder
 poll_csv = os.path.join('..', 'Resources', 'election_data.csv')
 # #02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv
@@ -15,14 +14,13 @@ with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     header = next(csvreader)
-    print(header)
+    #print(header)
 
-#create empty lists to store values of each column
+
+    #create empty lists to store values of each column
     CandName=[]
     County = []
     voterID = []
-    
-    #create empty lists to hold vote count for each candidate
 
     for row in csvreader:
 
@@ -33,33 +31,51 @@ with open(poll_csv, 'r') as csvfile:
 #print(totalvotecount)
 
 #print out list of individual candidate names by converting list to set
-NameList = set(CandName)      
-
+NameList = set(CandName)
 
 #initialize vote counter for each candidate name
 #name is each unique candidate name
 #cand in candlist is column to check against name list to find matches and add votes
+
+
+
+
+print("Election Results: ")
+print("---------------------------------------")
+print(f'Candidates who received votes: {NameList}')
+print(f'Total Votes: {totalvotecount}')
+print('---------------------------------------')
+resultlist = [] 
+ 
+
 def VoteSummary():
     for name in NameList:
         votecount=0
         for cand in CandName:
             if name == cand:
                 votecount+=1
-        print(f'{name}: {votecount}')
-    print("Election Results: ")
-    print("---------------------------------------")
-    print(f'Candidates who received votes: {NameList}')
-    print(f'Total Votes: {totalvotecount}')
-    print('---------------------------------------')
+                votepercent= round((votecount/totalvotecount)*100)
+                resultlist.append(votepercent)
 
+        print(f'{name}: {votecount} ({votepercent}%)')
+        
 
+    
 VoteSummary()
 
+# Specify the file to write to
+output_path = os.path.join("..", "Analysis", "new.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
 
 
+    csvfile.write("Election Results: ")
+    csvfile.write("---------------------------------------")
+    csvfile.write(f'Candidates who received votes: {NameList}')
+    csvfile.write(f'Total Votes: {totalvotecount}')
+    csvfile.write('---------------------------------------')
 
-#   * A complete list of candidates who received votes
-
-#   * The total number of votes each candidate won
-    
-#   * The percentage of votes each candidate won
